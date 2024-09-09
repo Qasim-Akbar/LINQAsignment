af61211028d601d67ae40eafbd8bfe2ab15f531e
AT <AT@DESKTOP-AKM319L>
2024-09-09 21:52:06 +0500

    Add project files.

---
+++ LINQAsignment/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQAsignment.Models
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
